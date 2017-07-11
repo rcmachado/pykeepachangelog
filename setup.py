0caaddc96758d4bcb43afa8da04e7d80e2f7f857
Rodrigo Machado <rcmachado@gmail.com>
2017-07-10 23:52:21 -0400

    Make package installable and add tox support

---
+++ setup.py
@@ -0,0 +1,42 @@
+from codecs import open
+from os import path
+from setuptools import setup, find_packages
+
+
+here = path.abspath(path.dirname(__file__))
+
+
+# Get the long description from the README file
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+
+setup(
+    name="pykeepachangelog",
+    description=("Parse and extract information from changelogs that follows "
+                 "the keepachangelog.com format"),
+    long_description=long_description,
+    url="https://github.com/rcmachado/pykeepachangelog",
+    author="Rodrigo Machado",
+    author_email="rcmachado@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: ",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Build Tools",
+    ],
+    keywords="changelog keepachangelog",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[
+        "misaka",
+    ],
+    extra_requires={
+        "test": ["pytest", "tox", "tox-pyenv"],
+    },
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+)
